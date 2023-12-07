dc8973078977bac13b9830cb487dddb535eb6289
Juliana Li <li.juliana4@gmail.com>
2023-12-07 03:28:43 -0500

    stay on land

--- src/js/EventHandlers.js
+++ src/js/EventHandlers.js
@@ -74,30 +74,34 @@ export function handleKeyUp(event){
 
 /******************** Handle Character Movements **********************/
 export function handleMovement(scene){
-    // const player = scene.getObjectByName("Trex_Original");
-    // // const player = scene.getObjectByName("Trex_Realistic");
-    // // const player = scene.getObjectByName("Trex_Cartoon");
-    // const floor_y = scene.getObjectByName("Land").position.y;
-    // // TODO: Modify transition objects and jump height to allow dino to jump over obstacles
-    // const jumpUp = new TWEEN.Tween(player.position).to({ y: floor_y + 3 }, 250 ).easing(TWEEN.Easing.Quadratic.Out);
-    // const fallDown = new TWEEN.Tween(player.position).to({ y: floor_y}, 400).easing(TWEEN.Easing.Quadratic.In);
+    var player_name = "Trex_" + scene.state.style;
+    const player = scene.getObjectByName(player_name);
+    const land = scene.getObjectByName("Land");
+    const floor_y = land.position.y;
+    // TODO: Modify transition objects and jump height to allow dino to jump over obstacles
+    const jumpUp = new TWEEN.Tween(player.position).to({ y: floor_y + 3 }, 250 ).easing(TWEEN.Easing.Quadratic.Out);
+    const fallDown = new TWEEN.Tween(player.position).to({ y: floor_y}, 400).easing(TWEEN.Easing.Quadratic.In);
     
-    // // Movement along the x-axis
-    // if(keys.left.pressed){
-    //     // Only move left if still on the land
-    //     player.position.x += 0.03;
-    // } else if (keys.right.pressed){
-    //     // Only move right if still on the land
-    //     player.position.x -= 0.03;
-    // } 
+    // Movement along the x-axis
+    if(keys.left.pressed){
+        // Only move left if still on the land
+        if (player.position.x + 0.03 <= land.scale.x + 1.5){
+            player.position.x += 0.03;   
+        }
+    } else if (keys.right.pressed){
+        // Only move right if still on the land
+        if (player.position.x - 0.03 >= -1 * land.scale.x - 1.5){
+            player.position.x -= 0.03;   
+        }
+    } 
     
-    // // Movement along the y-axis
-    // // Only jump if the dinosaur is on floor (prevents floating)
-    // if (keys.jump.pressed && player.position.y == floor_y){
-    //     // jumpUp.onComplete(() => fallDown.start());
-    //     // jumpUp.start();
-    //     // player.update();
-    // } else if (keys.down.pressed){
-    //     // TODO: make dinosaur fall down faster when down key is pressed
-    // }
+    // Movement along the y-axis
+    // Only jump if the dinosaur is on floor (prevents floating)
+    if (keys.jump.pressed && player.position.y == floor_y){
+        // jumpUp.onComplete(() => fallDown.start());
+        // jumpUp.start();
+        // player.update();
+    } else if (keys.down.pressed){
+        // TODO: make dinosaur fall down faster when down key is pressed
+    }
 }
