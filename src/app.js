d2f40ed0ce6f392a1466f18630002e6c19085136
Anika Agarwal <agarwal.anika@yahoo.com>
2023-12-14 04:16:09 -0500

    add camera angles

--- src/app.js
+++ src/app.js
@@ -31,6 +31,32 @@ const renderer = new WebGLRenderer({ antialias: true });
 /********************* Set Up Camera **********************************/
 camera.position.set(-3, 3, -10);
 camera.lookAt(new Vector3(0, 0, 5));
+function handleKeyDown(event){
+    if (scene.state.in_game){
+        if (event.key == "1"){
+            camera.position.set(-3, 3, -10);
+            camera.lookAt(new Vector3(0, 0, 5));
+        }
+        else if(event.key == "2"){
+            camera.position.set(0, 3, -10);
+            camera.lookAt(new Vector3(0, 0, 5));
+        }
+        else if(event.key == "3"){
+            camera.position.set(0, 2, -5);
+            camera.lookAt(new Vector3(0, 0, 100));
+        }
+        else if(event.key == "4"){
+            camera.position.set(0, 40, -5);
+            camera.lookAt(new Vector3(0, 0, -5));
+        }
+        else if (event.key == "5"){
+            camera.position.set(10, 3, -5);
+            camera.lookAt(new Vector3(-20, 3, -5));
+        }
+    }
+}
+document.addEventListener('keydown', handleKeyDown.bind(this));
+
 
 /*********** Set up renderer, canvas, and minor CSS adjustments *******/
 renderer.setPixelRatio(window.devicePixelRatio);
